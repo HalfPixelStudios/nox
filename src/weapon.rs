d17e4757ae4ca35b3b7c335e34e0f2b7cb0e3728
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-06-25 21:33:07 -0400

    sus macro stuff

--- src/weapon.rs
+++ src/weapon.rs
@@ -6,14 +6,14 @@ use super::{bullet::ShootFunction, *};
 #[derive(Component)]
 pub struct Weapon {
     pub name: String,
-    pub attack_fn: Fn(&mut Commands, Vec3, Vec2),
+    pub attack_fn: ShootFunction,
     pub attack_speed: f32, // time between consecutive attacks
 }
 
 pub fn steel_sword_prefab() -> Weapon {
     Weapon {
         name: "steel sword".to_string(),
-        attack_fn: attack::shotgun(bullet::player_bullet, 3, PI / 4.),
+        attack_fn: shotgun!(bullet::player_bullet, 3, PI / 4.),
         attack_speed: 1.,
     }
 }
@@ -21,7 +21,7 @@ pub fn steel_sword_prefab() -> Weapon {
 pub fn wooden_bow_prefab() -> Weapon {
     Weapon {
         name: "wooden bow".to_string(),
-        attack_fn: attack::straight(bullet::player_bullet),
+        attack_fn: straight!(bullet::player_bullet),
         attack_speed: 1.,
     }
 }
